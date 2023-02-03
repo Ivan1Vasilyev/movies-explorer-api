ce856b03460b5b5744320de7186ce10bdba921f9
Ivan Vasilyev <Nalivan3747@yandex.ru>
2023-02-04 00:54:49 +0300

    samesite=none secure=false

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,9 +6,8 @@ const jwtSign = (user, expiresIn) => jwt.sign({ _id: user._id }, tokenKey, { exp
 const setCookies = (maxAge) => ({
   maxAge,
   httpOnly: true,
-  sameSite: 'lax',
-  path: '/',
-  // secure: true,
+  sameSite: 'none',
+  secure: false,
 });
 
 const getErrorMessages = (e) => Object.values(e.errors)
